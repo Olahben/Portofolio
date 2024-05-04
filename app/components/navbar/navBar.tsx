fbf2a1f63726e8e19dda39ba71641fc74a058046
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-04 20:06:24 +0200

    create

---
+++ app/components/navbar/navBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const navBar = () => {
+  return (
+    <div className='navbar-center bg-component-background-color
+     flex w-full top-0 justify-center font-nunito font-medium text-md fixed pt-1 pb-1'></div>
+  )
+}
+
+export default navBar
