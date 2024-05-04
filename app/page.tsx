ce832362b73745d38ef71acdf449ebdf3a5341bd
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-04 20:04:22 +0200

    use navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import NavBar from "./components/navbar/navBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <NavBar />
     </main>
   );
 }
