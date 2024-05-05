a3d746247e4b3f1e2fa2e29e18295e1a6994552c
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-05 19:32:40 +0200

    use contact component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import NavBar from "./components/navbar/navBar";
+import Contact from "./components/contact/contact";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <NavBar />
+    <Contact />
     </main>
   );
 }
