c4b702a6d2d8a857781f97c00d6d9033556d173f
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-05 19:58:06 +0200

    set max width

--- app/components/navbar/navBar.tsx
+++ app/components/navbar/navBar.tsx
@@ -4,7 +4,7 @@ import NavBarLink from './navBarLink'
 const navBar = () => {
   return (
     <div className='navbar-center bg-component-background-color
-     flex w-full top-0 justify-between font-nunito font-medium text-md fixed p-[10%] pt-1.5 pb-1.5 sm:mt-5 sm:w-[45%] sm:pt-2.5 sm:pb-2.5 sm:p-[2%] sm:tracking-normal sm:text-md sm:rounded-full'>
+     flex w-full top-0 justify-between font-nunito font-medium text-md fixed p-[10%] pt-1.5 pb-1.5 sm:mt-5 sm:w-[45%] sm:pt-2.5 sm:pb-2.5 sm:p-[2%] sm:tracking-normal sm:text-md sm:rounded-full sm:max-w-xl'>
       <NavBarLink text="Home" />
       <NavBarLink text="Projects" />
       <NavBarLink text="Skills" />
