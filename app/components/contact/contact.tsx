9febbb0d310220f5ac8a5fcae51918541c5318cb
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-05 19:32:27 +0200

    create components

---
+++ app/components/contact/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactMe from './contactMe'
+
+const Contact = () => {
+  return (
+    <div className="mt-20 justify-center flex flex-wrap ml-4 mr-4 flex-col">
+        <ContactMe />
+    </div>
+  )
+}
+
+export default Contact
---
+++ app/components/contact/contactMe.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const ContactMe = () => {
+  return (
+    <>
+    <p className='text-3xl font-medium block text-center'>Contact Me</p>
+    <p className='text-center'>Please contact me directly at <Link href="mailto:olahben@icloud.com" className="underline">olahben@icloud.com</Link> or through the form below.</p>
+    </>
+  )
+}
+
+export default ContactMe
