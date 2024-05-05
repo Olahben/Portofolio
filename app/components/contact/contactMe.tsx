1f1910d08b2e669e3e2216a84b9eea0a14f342e3
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-05 21:22:49 +0200

    align text correctly

--- app/components/contact/contactMe.tsx
+++ app/components/contact/contactMe.tsx
@@ -4,8 +4,9 @@ import Link from 'next/link'
 const ContactMe = () => {
   return (
     <>
-    <p className='text-3xl font-medium block text-center'>Contact Me</p>
-    <p className='text-center'>Please contact me directly at <Link href="mailto:olahben@icloud.com" className="underline">olahben@icloud.com</Link> or through the form below.</p>
+      <p className='text-3xl font-medium block text-center'>Contact Me</p>
+      <p className='text-center'>Please contact me directly at <Link href="mailto:olahben@icloud.com" className="underline">olahben@icloud.com</Link> or</p>
+      <p className='text-center'> through the form below.</p>
     </>
   )
 }
