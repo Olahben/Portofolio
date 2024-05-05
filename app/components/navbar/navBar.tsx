afd6d551957109b5c290ae2c26d7bbee8a2e36d3
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-05 18:02:52 +0200

    Add links to different sections

--- app/components/navbar/navBar.tsx
+++ app/components/navbar/navBar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import NavBarLink from './navBarLink'
 
 const navBar = () => {
   return (
     <div className='navbar-center bg-component-background-color
-     flex w-full top-0 justify-center font-nunito font-medium text-md fixed pt-1 pb-1'></div>
+     flex w-full top-0 justify-between font-nunito font-medium text-md fixed p-[10%] pt-1.5 pb-1.5'>
+      <NavBarLink text="Home" />
+      <NavBarLink text="Projects" />
+      <NavBarLink text="Skills" />
+      <NavBarLink text="Contact" />
+     </div>
   )
 }
 
