023f9fe2a8b6d1ce602443b1290ab857a4f344b4
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-13 10:59:00 +0200

    get sender email and message

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -1,17 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import dotenv from 'dotenv';
 import nodemailer from "nodemailer";
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { json } from 'stream/consumers';
 
-export async function POST(req: NextApiRequest, {params}: {params: {email: string, message: string}}, res: NextApiResponse) {
+export async function POST(req: Request, res: NextApiResponse) {
     //res.status(200).send("Email sent successfully");
 if (req.method === 'POST') {
     const emailName = process.env.NEXT_PUBLIC_EMAIL_NAME;
     const password = process.env.NEXT_PUBLIC_OUTGOING_PASSWORD;
-    console.log(emailName ?? "NO EMAIL NAME");
-    console.log(password ?? "NO EMAIL NAME");
-    console.log(req.body.email);
-    console.log(req.body.message);
+    const data = await req.json();
+    const {email, message} = data;
+
     let transporter = nodemailer.createTransport({
         host: "smtp.mail.me.com",
         port: 587,
@@ -22,19 +22,19 @@ if (req.method === 'POST') {
         }
     });
 
-    var message = {
+    const emailMessage = {
         from: emailName,
         to: emailName,
         subject: "Someone is trying to contact you",
-        text: "test test",
-        html: `<p>test test</p>`,
-        replyTo: "test@icloud.com"
+        text: message,
+        html: `<p>${message}</p>`,
+        replyTo: email
     }
 
     try {
-   const info = await transporter.sendMail(message);
+   const info = await transporter.sendMail(emailMessage);
    console.log(info);
-   return NextResponse.json({data: info}, {status: 201})
+   return NextResponse.json({status: 201})
     } catch (err) {
         console.error("Error sending mail:", err)
         return NextResponse.json({error: err}, {status: 500})
