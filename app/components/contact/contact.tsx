cb125264cec39969ada34d21d42bfca68a4ad287
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-14 14:24:03 +0200

    Edit media queries

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -4,9 +4,9 @@ import Form from './form'
 
 const Contact = () => {
   return (
-    <div className="justify-start flex flex-wrap flex-col sm:max-w-[608] sm:w-[608px]">
-        <ContactMe />
-        <Form />
+    <div className="justify-start flex flex-wrap flex-col sm:max-w-[608px] sm:w-[608px] md:max-w-[740px] md:w-[608px] lg:max-w-[908px] lg:w-[608px]">
+      <ContactMe />
+      <Form />
     </div>
   )
 }
