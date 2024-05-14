90cb277a30107e49236f5871f757aeadd74ba9e5
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-14 13:25:30 +0200

    Create

--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import SkillIcons from './SkillIcons'
 
 const Skills = () => {
   return (
-    <div className=''>layout</div>
+    <div className='sm:max-w-[608px] sm:w-[608px] flex justify-center text-center flex-wrap flex-col'>
+      <p className="text-3xl font-medium mb-8">My Skills</p>
+      <ul className="flex flex-wrap justify-center gap-0.5 gap-y-3">
+        <SkillIcons />
+      </ul>
+    </div>
   )
 }
 
