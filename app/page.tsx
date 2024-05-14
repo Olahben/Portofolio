e3ee49e0bf1804a49723a594192f71294e8ef9ff
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-14 11:33:30 +0200

    Fix general page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import NavBar from "./components/navbar/navBar";
 import Contact from "./components/contact/contact";
+import Skills from "./components/skills/layout";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center gap-14 pt-[40px] sm:pt-[68px]">
       <NavBar />
+      <Skills />
     <Contact />
     </main>
   );
