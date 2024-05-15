e649b7d52517f0818646f83b7710fdf0ce9d59ce
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-15 20:00:58 +0200

    Define breakpoints

---
+++ app/components/hero/hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BgParticles from './particles'
+
+const Hero = () => {
+    return (
+        <div className="w-[100%] mt-[-76px] sm:h-[580px] md:h-[624px] xl:h-[560px]">
+            <BgParticles />
+        </div>
+    )
+}
+
+export default Hero
