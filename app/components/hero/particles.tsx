f5a3477551c302cc7fe53404b438d5157bf3ef3f
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-15 18:31:03 +0200

    create

---
+++ app/components/hero/particles.tsx
@@ -0,0 +1,93 @@
+"use client"
+import React from 'react'
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { tsParticles } from '@tsparticles/engine';
+import {
+    type Container,
+    type ISourceOptions,
+    MoveDirection,
+    OutMode,
+    Size,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const hero = () => {
+    const [init, setInit] = useState(false);
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = async (container?: Container): Promise<void> => {
+        console.log(container);
+    };
+
+    const options: ISourceOptions = useMemo(
+        () => ({
+            background: {
+                color: {
+                    value: "#0d47a1",
+                },
+            },
+            particles: {
+                color: {
+                    value: "#fff"
+                },
+                number: {
+                    density: {
+                        enable: true,
+                    },
+                    value: 80,
+                },
+                opacity: {
+                    value: { min: 0.3, max: 1 }
+                },
+                shape: {
+                    type: "circle"
+                },
+                size: {
+                    value: { min: 1, max: 5 }
+                },
+                links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                },
+                move: {
+                    direction: MoveDirection.none,
+                    enable: true,
+                    outModes: {
+                        default: OutMode.out,
+                    },
+                    random: false,
+                    speed: 6,
+                    straight: false,
+                },
+            },
+            detectRetina: true,
+            fullScreen: false,
+        }),
+        [],
+    );
+
+    if (init) {
+        return (
+            <Particles
+                id="tsparticles"
+                particlesLoaded={particlesLoaded}
+                options={options}
+                className="w-[100%] h-[100%]"
+            />
+        );
+    }
+
+    return <></>;
+}
+
+export default hero
