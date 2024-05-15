ddb59e63f1fa88b4f21b0212ae1fb310e2a62bf0
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-15 20:55:18 +0200

    create

---
+++ app/components/hero/Me.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Me = () => {
+    return (
+        <div className="flex flex-col w-full h-full absolute items-center mt-32">
+            <div className="avatar">
+                <div className="w-28 rounded-full ring ring-primary ring-offset-base-300">
+                    <img src="/IMG_1732.jpg"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Me
