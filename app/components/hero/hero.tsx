22acab5992a059cfd0dd3488d2f732cabc439a05
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-16 18:49:12 +0200

    Add me section

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import BgParticles from './particles'
+import Me from './Me'
+import SocialLinks from './SocialLinks'
 
 const Hero = () => {
     return (
-        <div className="w-[100%] mt-[-76px] sm:h-[580px] md:h-[624px] xl:h-[560px]">
+        <div className="w-[100%] mt-[-76px] h-[560px] sm:h-[580px] md:h-[624px] xl:h-[560px] relative z-[0] flex justify-center">
             <BgParticles />
+            <Me />
         </div>
     )
 }
