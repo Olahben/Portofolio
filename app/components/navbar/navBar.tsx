a87c2bc81762274c4e6560d6eeb1ff7aa84cedac
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-16 18:49:36 +0200

    Fix z-index

--- app/components/navbar/navBar.tsx
+++ app/components/navbar/navBar.tsx
@@ -3,10 +3,10 @@ import NavBarLink from './navBarLink'
 
 const navBar = () => {
   return (
-    <div className='navbar-center bg-component-background-color
+    <div className="navbar-center bg-component-background-color
      flex w-full top-0 justify-between font-nunito text-md fixed p-[10%] pt-1.5 pb-1.5 sm:mt-5
       sm:pt-2.5 sm:pb-2.5 sm:p-[2%] sm:tracking-normal sm:text-lg sm:rounded-full sm:max-w-[608px]
-      sm:font-[200] sm:pl-8 sm:pr-8'>
+      sm:font-[200] sm:pl-8 sm:pr-8 z-10">
       <NavBarLink text="Home" />
       <NavBarLink text="Projects" />
       <NavBarLink text="Skills" />
