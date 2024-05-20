38334fedf3085507665d6dd5a807751afcccbd94
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-20 21:02:16 +0200

    Use lagerhotell, create

---
+++ app/components/projects/Projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Lagerhotell from './Lagerhotell'
+
+const Projects = () => {
+    return (
+        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5">
+            <p className="text-3xl font-medium mb-4">My Projects</p>
+            <Lagerhotell />
+        </div>
+    )
+}
+
+export default Projects
