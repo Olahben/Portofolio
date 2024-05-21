719ec1a67df773f7b4eec401358040b69d0ee577
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-21 13:25:29 +0200

    Include Hero and projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import NavBar from "./components/navbar/navBar";
 import Contact from "./components/contact/contact";
-import Skills from "./components/skills/layout";
+import Skills from "./components/skills/Skills";
+import Hero from "./components/hero/hero";
+import Projects from "./components/projects/Projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-14 pt-[40px] sm:pt-[68px]">
       <NavBar />
+      <Hero />
+      <Projects />
       <Skills />
-    <Contact />
+      <Contact />
     </main>
   );
 }
