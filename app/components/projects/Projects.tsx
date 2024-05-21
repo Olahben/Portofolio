bfe3d9ae69c0f77539052890fc77a3fbbeadc713
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-21 13:07:27 +0200

    add gap between projects

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Lagerhotell from './Lagerhotell'
+import CryptoBot from './CryptoBot'
 
 const Projects = () => {
     return (
-        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5">
+        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5 flex flex-col gap-y-10">
             <p className="text-3xl font-medium mb-4">My Projects</p>
             <Lagerhotell />
+            <CryptoBot />
         </div>
     )
 }
