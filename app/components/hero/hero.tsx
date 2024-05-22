908be155b74253b3a441772c7fb9c318d657aa29
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-22 15:49:36 +0200

    Set id

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -5,7 +5,7 @@ import SocialLinks from './SocialLinks'
 
 const Hero = () => {
     return (
-        <div className="w-[100%] mt-[-76px] h-[560px] sm:h-[580px] md:h-[624px] xl:h-[560px] relative z-[0] flex justify-center">
+        <div className="w-[100%] mt-[-76px] h-[560px] sm:h-[580px] md:h-[624px] xl:h-[560px] relative z-[0] flex justify-center" id="Home">
             <BgParticles />
             <Me />
         </div>
--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -6,7 +6,7 @@ import Portfolio from './Portfolio'
 
 const Projects = () => {
     return (
-        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5 flex flex-col gap-y-10">
+        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5 flex flex-col gap-y-10" id="Projects">
             <p className="text-3xl font-medium mb-4">My Projects</p>
             <Lagerhotell />
             <CryptoBot />
