7a883d066adc31d626993a6af525ac987acd5d33
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-25 22:22:58 +0200

    create transition on selected section background

--- app/components/navbar/navBarLink.tsx
+++ app/components/navbar/navBarLink.tsx
@@ -9,7 +9,7 @@ type props = {
 
 const NavBarLink: React.FC<props> = ({ text, currentSection }) => {
   return (
-    <span className={`rounded-full group p-2 px-3 ${currentSection === text ? "bg-white" : ""}`}>
+    <span className={`rounded-full group p-2 px-3 ${currentSection === text ? "bg-slate-100 transition ease-in-out duration-300" : ""}`}>
       <Link className={currentSection === text ? "text-black text-[16px]" : "hover:text-black text-[rgb(107,114,128)] text-[16px]"} href={`#${text}`
       }> {text}</Link >
     </span >
