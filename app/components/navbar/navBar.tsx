d0ae4a2dd0817471a1d60b91c0ab583bb12e8404
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-25 18:39:36 +0200

    Add background to navBarLink to indicate which section user is on

--- app/components/navbar/navBar.tsx
+++ app/components/navbar/navBar.tsx
@@ -1,19 +1,48 @@
+"use client"
 import React from 'react'
 import NavBarLink from './navBarLink'
+import { useEffect, useState } from 'react'
 
-const navBar = () => {
+type NavBarLinkProps = {
+  text: string;
+  currentSection: string;
+}
+
+export default function NavBar() {
+  const [currentSection, setCurrentSection] = useState("");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setCurrentSection(entry.target.id)
+        }
+      });
+    }, { threshold: 0.7 });
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach(section => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
+
   return (
+
     <div className="navbar-center bg-component-background-color
-     flex w-full top-0 justify-between font-nunito text-md fixed p-[10%] pt-1.5 pb-1.5 sm:mt-5
+     flex w-full top-0 justify-between font-nunito text-md fixed p-[10%] sm:mt-5
       sm:pt-2.5 sm:pb-2.5 sm:p-[2%] sm:tracking-normal sm:text-lg sm:rounded-full sm:max-w-[608px]
       sm:font-[200] sm:pl-8 sm:pr-8 z-10">
-      <NavBarLink text="Home" />
-      <NavBarLink text="Projects" />
-      <NavBarLink text="Skills" />
-      <NavBarLink text="Experience" />
-      <NavBarLink text="Contact" />
+      <NavBarLink text="Home" currentSection={currentSection} />
+      <NavBarLink text="Projects" currentSection={currentSection} />
+      <NavBarLink text="Skills" currentSection={currentSection} />
+      <NavBarLink text="Experience" currentSection={currentSection} />
+      <NavBarLink text="Contact" currentSection={currentSection} />
     </div>
   )
 }
-
-export default navBar
--- app/components/navbar/navBarLink.tsx
+++ app/components/navbar/navBarLink.tsx
@@ -2,14 +2,18 @@
 import React from 'react'
 import Link from 'next/link'
 
-type TextLink = {
+type props = {
   text: string;
+  currentSection: string;
 }
 
-const NavBarLink: React.FC<TextLink> = ({ text }) => {
+const NavBarLink: React.FC<props> = ({ text, currentSection }) => {
   return (
-    <Link className="hover:text-black text-[rgb(107,114,128)] text-[16px]" href={`#${text}`}>{text}</Link>
+    <span className={`rounded-full group p-2 px-3 ${currentSection === text ? "bg-white" : ""}`}>
+      <Link className={currentSection === text ? "text-black text-[16px]" : "hover:text-black text-[rgb(107,114,128)] text-[16px]"} href={`#${text}`
+      }> {text}</Link >
+    </span >
   )
 }
 
-export default NavBarLink
+export default NavBarLink;
