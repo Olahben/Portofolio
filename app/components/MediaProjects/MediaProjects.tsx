8ecc57ad69171148ca4425be3a9495f9526828fe
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-28 14:09:57 +0200

    Adjust content based on screen size

--- app/components/MediaProjects/MediaProjects.tsx
+++ app/components/MediaProjects/MediaProjects.tsx
@@ -1,16 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const MediaProjects = () => {
+    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth > 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     return (
         <div className="max-w-[1064px] text-center p-2">
             <p className="text-3xl font-medium mb-5">Media-oriented projects</p>
-            <div className="flex flex-row flex-wrap max-w-[1064px] p-2 gap-x-10 gap-y-4 justify-center">
-                <img alt="Shopping mall" src="/kjopeSenter1.jpeg" className="w-[40%] order-1"></img>
-                <img alt="My friend" src="/yevhenii.jpg" className="w-[40%] order-2"></img>
-                <img alt="Nature" src="/natur.jpg" style={{ width: 'calc(80% + 2.5rem)' }} className=" order-3"></img>
-                <img alt="Move I produced" src="lidenskap.png" style={{ width: 'calc(80% + 2.5rem)' }} className="] order-4"></img>
+            <div className="flex flex-row flex-wrap max-w-[1064px] p-2 gap-x-20 gap-y-4 justify-center md:gap-x-3 md:gap-y-3">
+                <img alt="Shopping mall" src="/kjopeSenter1.jpeg" className="w-[40%] order-1 md:order-1 md:w-[33%]"></img>
+                <img alt="My friend" src="/yevhenii.jpg" className="w-[40%] order-2 md:order-3 md:w-[28%]"></img>
+                <img alt="Nature" src="/natur.jpg" style={isLargeScreen ? {} : { width: 'calc(80% + 5rem)' }} className="order-3 md:order-2 md:w-[65%]"></img>
+                <img alt="Move I produced" src="lidenskap.png" style={isLargeScreen ? {} : { width: 'calc(80% + 5rem)' }} className="order-4 md:order-4 md:w-[70%]"></img>
             </div>
         </div>
     )
