b4d37a0b85761293323b8d8c68d0cf8f835b368e
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-29 19:22:33 +0200

    Fix responsiveness for bigger screens

--- app/components/MediaProjects/MediaProjects.tsx
+++ app/components/MediaProjects/MediaProjects.tsx
@@ -19,20 +19,20 @@ const MediaProjects = () => {
         <div className="max-w-[1064px] text-center p-2">
             <p className="text-2xl font-normal mb-5">Media-oriented projects</p>
             <div className="flex flex-row flex-wrap max-w-[1064px] p-2 gap-x-20 gap-y-4 justify-center md:gap-x-3 md:gap-y-3">
-                <div className="flex flex-row flex-wrap max-w-[1064px] p-2 gap-x-10 gap-y-2 justify-center">
-                    <div className="relative group w-[35%] order-1 md:order-1">
+                <div className="flex flex-row flex-wrap max-w-[1064px] p-2 gap-x-10 md:gap-x-2 gap-y-2 justify-center">
+                    <div className="relative group w-[35%] md:w-[33%] order-1 md:order-1">
                         <img src="/kjopeSenter1.jpeg" className="w-full h-full"></img>
                         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-200"></div>
                     </div>
-                    <div className="relative group] order-3 md:order-2" style={isLargeScreen ? { width: "69%" } : { width: "calc(70% + 2.5rem)" }}>
+                    <div className="relative group order-3 md:order-2" style={isLargeScreen ? { width: "62%" } : { width: "calc(70% + 2.5rem)" }}>
                         <img src="/natur.jpg" className="w-full h-full"></img>
                         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-200"></div>
                     </div>
-                    <div className="relative group w-[35%] order-2 md:order-3">
+                    <div className="relative group w-[35%] md:w-[31%] order-2 md:order-3">
                         <img src="/yevhenii.jpg" className="w-full h-full"></img>
                         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-200"></div>
                     </div>
-                    <div className={`relative group order-4 md:order-4`} style={isLargeScreen ? { width: "69%" } : { width: "calc(70% + 2.5rem)" }}>
+                    <div className={`relative group order-4 md:order-4`} style={isLargeScreen ? { width: "64%" } : { width: "calc(70% + 2.5rem)" }}>
                         <img src="lidenskap.png" className="w-full h-full"></img>
                         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-200 text-white text-base p-2">
                             <p>A short movie covering my friend Håkons interest about music. I used a lot of different cameras and equipment to get the right content. I used Adobe Premiere Pro to edit the material and get my desired result.</p>
