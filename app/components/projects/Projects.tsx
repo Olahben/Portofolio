3dd3cce0429a60efc530d852923e9f7d12ea3808
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-29 18:55:03 +0200

    Reduce top margin to fix animation

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -6,7 +6,7 @@ import Portfolio from './Portfolio'
 
 const Projects = () => {
     return (
-        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5 flex flex-col gap-y-10" data-aos="fade-up" data-aos-duration="2000">
+        <div className="max-w-[56rem] flex flex-col content-center justify-center text-center m-5 mt-0 flex flex-col gap-y-10" data-aos="fade-up" data-aos-duration="2000">
             <p className="text-3xl font-medium mb-4">My Projects</p>
             <Lagerhotell />
             <CryptoBot />
